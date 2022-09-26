32701392a1cda9ea04c1732586400e008662a8b4
Anant1711 <anakota9962@gmail.com>
2022-09-26 11:32:05 +0530

    Transactions file added

--- Transactions.java
+++ Transactions.java
@@ -1,18 +1,43 @@
-
+//Transactions body in Blockchain
 public class Transactions {
     private String source;
     private String destination;
-    private long Money;
+    private String Money;
 
-    Transactions(String s, String d, long m){
+    Transactions(String s, String d, String m){
         this.Money = m;
         this.source=s;
         this.destination=d;
     }
 
-    public String getSource(){return this.source;}
-    public void setSource(String s){this.source=s;}
-    public String getDest(){return this.destination;}
+    @Override
+    public int hashCode() {
+        int hash = source!=null ? source.hashCode() : 0;
+        hash = 31*hash+(destination!=null ? destination.hashCode() : 0);
+        hash = 31*hash+(Money!=null ? Money.hashCode():0);
 
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transactions that = (Transactions) o;
+
+        if (source != null ? !source.equals(that.source) : that.source != null) return false;
+        if (destination != null ? !destination.equals(that.destination) : that.destination != null)
+            {return false;}
+        
+        if(Money!=null){return Money.equals(Money);
+        }else{
+            return Money==null;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Source: "+this.source+" Destination: "+this.destination+" Amount: "+this.Money;
+    }
     
 }
