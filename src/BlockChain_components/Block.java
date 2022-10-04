411f607f49ad42cd790a11c10eb126cb8a6e27a7
Anant1711 <anakota9962@gmail.com>
2022-10-04 22:28:52 +0530

    Block components

---
+++ src/BlockChain_components/Block.java
@@ -0,0 +1,44 @@
+package BlockChain_components; /**
+ * BlockChain_components.Block Body in BlockChain
+ */
+
+import java.util.List;
+public class Block {
+
+    //private int blocknumber;
+    private String previousHash;
+    private List<Transactions> l;
+
+    public Block(List<Transactions> t1, String previousHash){
+        this.previousHash=previousHash;
+        this.l=t1;
+    }
+
+    public String getPrevhash(){return previousHash;}
+    public List<Transactions> getTransDet(){return l;}
+
+    //We override HashCode and equal method because it will not give the same hash everytime we do change in transaction.
+    @Override
+    public int hashCode() {
+        int hash = previousHash!=null ? previousHash.hashCode():0;
+        hash = 32*hash + (l!=null ? l.hashCode():0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Block block = (Block) o;
+
+        if (previousHash != block.previousHash) return false;
+        return l != null ? l.equals(block.l) : block.l == null;
+    }
+
+    @Override
+    public String toString() {
+        return "Previous Hash: "+this.previousHash+", Transaction: "+l+", ";
+    }
+
+}
---
+++ src/BlockChain_components/Transactions.java
@@ -0,0 +1,45 @@
+package BlockChain_components;
+
+//BlockChain_components.Transactions body in Blockchain
+public class Transactions {
+    private String source;
+    private String destination;
+    private String Money;
+
+    public Transactions(String s, String d, String m){
+        this.Money = m;
+        this.source=s;
+        this.destination=d;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = source!=null ? source.hashCode() : 0;
+        hash = 31*hash+(destination!=null ? destination.hashCode() : 0);
+        hash = 31*hash+(Money!=null ? Money.hashCode():0);
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transactions that = (Transactions) o;
+
+        if (source != null ? !source.equals(that.source) : that.source != null) return false;
+        if (destination != null ? !destination.equals(that.destination) : that.destination != null)
+        {return false;}
+
+        if(Money!=null){return Money.equals(Money);
+        }else{
+            return Money==null;
+        }
+    }
+    @Override
+    public String toString() {
+        return "From: "+this.source+", To: "+this.destination+", Amount: "+this.Money;
+    }
+
+}
